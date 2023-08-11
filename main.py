c6a5154003274eab6928fc219c206bc0f1371d8b
njulia2 <njulia2@vt.edu>
2023-08-11 18:10:18 +0100

    AES256, GCM mode
    incorporated AES test cases for matrix mult and log reg, for floats and integers
    no longer CBC mode since CEC is less secure and slower

--- main.py
+++ main.py
@@ -9,7 +9,31 @@ from timeit import default_timer as timer
 import argparse 
 import json
 
+# AES 256 encryption/decryption using pycryptodome library
+# https://cryptobook.nakov.com/symmetric-key-ciphers/aes-encrypt-decrypt-examplesfrom Crypto.Cipher import AES
+from Crypto.Cipher import AES
+from Crypto import Random
 
+'''
+    Helper functions for AES256 Encryption, GCM mode
+'''
+def get_secret_key():
+    return Random.get_random_bytes(32) # 256-bit random encryption key
+
+def encrypt_AES_GCM(msg, secretKey):
+    aesCipher = AES.new(secretKey, AES.MODE_GCM)
+    ciphertext, authTag = aesCipher.encrypt_and_digest(msg)
+    return (ciphertext, aesCipher.nonce, authTag)
+
+def decrypt_AES_GCM(encryptedMsg, secretKey):
+    (ciphertext, nonce, authTag) = encryptedMsg
+    aesCipher = AES.new(secretKey, AES.MODE_GCM, nonce)
+    plaintext = aesCipher.decrypt_and_verify(ciphertext, authTag)
+    return plaintext
+
+'''
+    Standard Matrix Multiplication - Float
+'''
 def run_mat_mulf(n: int, m: int, scale: int) -> float:
     a = np.random.random((n, m)) * scale
     b = np.random.random((m, n)) * scale
@@ -19,6 +43,9 @@ def run_mat_mulf(n: int, m: int, scale: int) -> float:
     return stop - start
 
 
+'''
+    Standard Matrix Multiplication - Integer
+'''
 def run_mat_muli(n: int, m: int, scale: int) -> float:
     a = np.random.randint(scale, size=(n, m))
     b = np.random.randint(scale, size=(m, n))
@@ -28,7 +55,92 @@ def run_mat_muli(n: int, m: int, scale: int) -> float:
     return stop - start
 
 
+'''
+    AES Matrix Multiplication - Float
+'''
+def run_mat_mulf_aes(n: int, m: int, scale: int) -> float:
+    secretKey = get_secret_key()
+    a = np.random.random((n, m)) * scale
+    b = np.random.random((m, n)) * scale
+    start = timer()
+    
+    # encrypting matrices, a and b
+    a_encrypt = encrypt_AES_GCM(a.tobytes(), secretKey)
+    b_encrypt = encrypt_AES_GCM(b.tobytes(), secretKey)
 
+    # decrypting encrypted matrices
+    a_decrypt = np.frombuffer(decrypt_AES_GCM(a_encrypt, secretKey))
+    a_decrypt.resize((a.shape)) 
+    b_decrypt = np.frombuffer(decrypt_AES_GCM(b_encrypt, secretKey))
+    b_decrypt.resize((b.shape))
+
+    res = a_decrypt @ b_decrypt
+
+    stop = timer()
+    return stop - start
+
+
+'''
+    Standard Matrix Multiplication - Integer
+'''
+def run_mat_muli_aes(n: int, m: int, scale: int) -> float:
+    secretKey = get_secret_key()
+    a = np.random.randint((n, m)) * scale
+    b = np.random.randint((m, n)) * scale
+    start = timer()
+
+    # encrypting matrices, a and b
+    a_encrypt = encrypt_AES_GCM(a.tobytes(), secretKey)
+    b_encrypt = encrypt_AES_GCM(b.tobytes(), secretKey)
+
+    # decrypting encrypted matrices
+    a_decrypt = np.frombuffer(decrypt_AES_GCM(a_encrypt, secretKey), dtype=int)
+    a_decrypt.resize((a.shape)) 
+    b_decrypt = np.frombuffer(decrypt_AES_GCM(b_encrypt, secretKey), dtype=int)
+    b_decrypt.resize((b.shape))
+
+    res = a_decrypt @ b_decrypt
+
+    stop = timer()
+    return stop - start
+
+
+'''
+    FHE Matrix Multiplication - Float
+'''
+def run_mat_mulf_fhe(n: int, m: int, scale: int, params=None) -> float:
+    HE = Pyfhel()
+    if params is not None: 
+        ckks_params = params
+    else:
+        ckks_params = {
+            "scheme": "CKKS",
+            "n": 2 ** 14,
+            "scale": 2 ** 30,
+            "qi_sizes": [60, 30, 30, 30, 60]
+        }
+    HE.contextGen(**ckks_params)
+    HE.keyGen()
+    HE.relinKeyGen()
+    HE.rotateKeyGen()
+    a_mat = np.random.random((n, m)) * scale
+    b_mat = (np.random.random((m, n)) * scale)
+    a_enc = [HE.encryptFrac(np.array(row)) for row in a_mat]
+    b_enc = [HE.encryptFrac(np.array(col)) for col in b_mat.T]
+    start = timer()
+    res = []
+    for a_row in a_enc:
+        sub_res = []
+        for b_col in b_enc:
+            sub_res.append(HE.scalar_prod(a_row, b_col, in_new_ctxt=True))
+        res.append(sub_res)
+    stop = timer()
+    return stop - start
+
+
+'''
+    FHE Matrix Multiplication - Integer
+'''
 def run_mat_muli_fhe(n: int, m: int, scale: int, params=None) -> float:
     HE = Pyfhel()
     if params is not None: 
@@ -62,36 +174,9 @@ def run_mat_muli_fhe(n: int, m: int, scale: int, params=None) -> float:
     return stop - start
 
 
-def run_mat_mulf_fhe(n: int, m: int, scale: int, params=None) -> float:
-    HE = Pyfhel()
-    if params is not None: 
-        ckks_params = params
-    else:
-        ckks_params = {
-            "scheme": "CKKS",
-            "n": 2 ** 14,
-            "scale": 2 ** 30,
-            "qi_sizes": [60, 30, 30, 30, 60]
-        }
-    HE.contextGen(**ckks_params)
-    HE.keyGen()
-    HE.relinKeyGen()
-    HE.rotateKeyGen()
-    a_mat = np.random.random((n, m)) * scale
-    b_mat = (np.random.random((m, n)) * scale)
-    a_enc = [HE.encryptFrac(np.array(row)) for row in a_mat]
-    b_enc = [HE.encryptFrac(np.array(col)) for col in b_mat.T]
-    start = timer()
-    res = []
-    for a_row in a_enc:
-        sub_res = []
-        for b_col in b_enc:
-            sub_res.append(HE.scalar_prod(a_row, b_col, in_new_ctxt=True))
-        res.append(sub_res)
-    stop = timer()
-    return stop - start
-
-
+'''
+    Standard Logistic Regression
+'''
 def run_logistic_reg() -> float:
     data, target = datasets.load_iris(return_X_y=True)
     data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=.5, random_state=None)
@@ -108,6 +193,46 @@ def run_logistic_reg() -> float:
     return stop - start
 
 
+'''
+    AES Encryption Logistic Regression
+'''
+def run_logistic_reg_aes() -> float:
+    secretKey = get_secret_key()
+    data, target = datasets.load_iris(return_X_y=True)
+    data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=.5, random_state=None)
+    
+    # preprocess data
+    scaler = preprocessing.StandardScaler().fit(data_train)
+    data_train = scaler.transform(data_train)
+    data_test = scaler.transform(data_test)
+    lin_reg = LogisticRegression()
+    
+    # run inference
+    start = timer()
+    
+    # encrypt test data, train data, target train
+    encrypted_test_data = encrypt_AES_GCM(data_test.tobytes(), secretKey)
+    decrypted_test_data = np.frombuffer(decrypt_AES_GCM(encrypted_test_data, secretKey))
+    decrypted_test_data.resize((data_test.shape)) 
+
+    encrypted_train_data = encrypt_AES_GCM(data_train.tobytes(), secretKey)
+    decrypted_train_data = np.frombuffer(decrypt_AES_GCM(encrypted_train_data, secretKey))
+    decrypted_train_data.resize((data_train.shape))
+
+    encrypted_target_train = encrypt_AES_GCM(target_train.tobytes(), secretKey)
+    decrypted_target_train = np.frombuffer(decrypt_AES_GCM(encrypted_target_train, secretKey), dtype=int)
+    decrypted_target_train.resize((target_train.shape))    
+
+    # y pred based on aes decrypted values
+    y_pred = lin_reg.fit(decrypted_train_data, decrypted_target_train).predict(decrypted_test_data)
+    stop = timer()
+    print("Accuracy AES:", metrics.accuracy_score(target_test, y_pred))
+    return stop - start   
+
+
+'''
+    FHE Logistic Regression
+'''
 def run_logistic_reg_fhe(params=None) -> float:
     data, target = datasets.load_iris(return_X_y=True)
 
@@ -169,14 +294,23 @@ def main(args):
             logger.write(f'Running Test: {cnt}, {test["desc"]}\n')
             func = None 
             args = [] 
-            if test["type"] == "log_reg": 
-                func = run_logistic_reg_fhe if test["encryption"] == "FHE" else run_logistic_reg 
+            if test["type"] == "log_reg":
+                if test["encryption"] == "FHE": 
+                    func = run_logistic_reg_fhe 
+                elif test["encryption"] == "AES":
+                    func = run_logistic_reg_aes 
+                else:
+                    func = run_logistic_reg 
 
             elif test["type"] == "mat_mul": 
                 if test["encryption"] == "FHE" and test["data_type"] == "float": 
                     func = run_mat_mulf_fhe 
                 elif test["encryption"] == "FHE" and test["data_type"] == "int":
                     func = run_mat_muli_fhe 
+                elif test["encryption"] == "AES" and test["data_type"] == "float":
+                    func = run_mat_mulf_aes 
+                elif test["encryption"] == "AES" and test["data_type"] == "int":
+                    func = run_mat_muli_aes 
                 elif test["encryption"] == "NONE" and test["data_type"] == "float":
                     func = run_mat_mulf
                 elif test["encryption"] == "NONE" and test["data_type"] == "int": 
